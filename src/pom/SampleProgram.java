5c87a6ee23770c31ed050aa46ec2f19bdacdc829
Narendar123 <chilukoti.narendar@gmail.com>
2018-02-15 10:26:17 +0530

    its local master push the code

---
+++ src/pom/SampleProgram.java
@@ -0,0 +1,14 @@
+package pom;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=15;
+		int c=a+b;
+		System.out.println("adition value is::" +c);
+		
+	}
+
+}
